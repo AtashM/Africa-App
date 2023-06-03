9555200d6d5147b7d7e1f8ae0a24950d041775b6
Atash Musazada <a.musazada@gmail.com>
2023-06-03 17:39:19 +0400

    Implement JSON decoding extension SwiftUI

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Atash Musazada on 03.06.23.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
