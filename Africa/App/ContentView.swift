c53f89db74be7c1170bffb16bb96c9c8d9aa0b8a
Atashhh <atash.musazade@gmail.com>
2023-06-04 16:23:53 +0400

    detail view for each animal with dynamic data - Part 1

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -19,9 +19,14 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
                 ForEach(animals) { animal in
-                    AnimalListItemView(animal: animal)
-                }
+                    
+                    NavigationLink(destination: AnimalDetailView(animal: animal)){
+                        AnimalListItemView(animal: animal)
+                        
+                    }//: NAVIGATION
+                }//: LOOP
             }//: LIST
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
